0addcb6f8060b28fcc87f036e3800c29afc3f401
astwys <astwys@gmail.com>
2017-09-18 16:54:18 +0200

    DLN-64 #time 30m added cors to accept requests from the frontend

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import koa from 'koa'
 import cors from 'kcors'
 import koaRouter from 'koa-router'
 import koaBody from 'koa-bodyparser'
+import convert from 'koa-convert'
 import { execute, subscribe } from 'graphql'
 import { createServer } from 'http'
 import { SubscriptionServer } from 'subscriptions-transport-ws'
@@ -12,6 +13,16 @@ import schema from './graphql/schema'
 const app = new koa()
 const router = new koaRouter()
 const PORT = 8080
+
+const whitelist = [`http://localhost:${PORT}`, 'http://localhost:3000']
+
+function checkOriginAgainstWhitelist(ctx) {
+	const requestOrigin = ctx.accept.headers.origin
+	if (!whitelist.includes(requestOrigin)) {
+		return ctx.throw(`🙈 ${requestOrigin} is not a valid origin`)
+	}
+	return requestOrigin
+}
 
 app.use(koaBody())
 
@@ -30,13 +41,9 @@ router.get(
 	}),
 )
 
-app.use(router.routes())
 app.use(router.allowedMethods())
-app.use(
-	cors({
-		origin: `http://localhost:${PORT}`,
-	}),
-)
+app.use(convert(cors({ origin: checkOriginAgainstWhitelist })))
+app.use(router.routes())
 
 const ws = createServer(app.callback())
 ws.listen(PORT, () => {
