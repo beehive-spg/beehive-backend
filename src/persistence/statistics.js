0f69be7f3bc0fb52ec54304539fbebde01b05e8a
Michael Bartl <astwys@gmail.com>
2018-03-08 08:06:23 +0100

    add end time for statistics

--- src/persistence/statistics.js
+++ src/persistence/statistics.js
@@ -1,9 +1,12 @@
-import { format, addHours } from 'date-fns'
+import { format, addHours, addMinutes } from 'date-fns'
 import { axiosInstance } from 'server'
 
 const getStatistics = async id => {
-	const time = format(addHours(Date.now(), 1), 'x')
-	const data = await axiosInstance.get(`/api/statistics/${id}/${time}`)
+	const start = format(addHours(Date.now(), 1), 'x')
+	const end = format(addHours(addMinutes(Date.now(), 5), 1), 'x')
+	const data = await axiosInstance.get(
+		`/api/statistics/${id}/${start}/${end}`,
+	)
 	return data.data
 }
 
