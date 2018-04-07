55e29d7fd8c2a15f74165b6fa1f7496191ecebba
Michael Bartl <astwys@gmail.com>
2018-04-07 18:53:17 +0200

    add rabbitmq settings exchange & queues

--- src/controller/settings.js
+++ src/controller/settings.js
@@ -4,6 +4,7 @@ import {
 	postRouting,
 	postDrones,
 } from 'persistence/settings'
+import { produceSetting } from 'connect/producers'
 
 const settings = async () => {
 	const objects = await getSettings()
@@ -17,20 +18,33 @@ const settings = async () => {
 
 const setDist = async value => {
 	const object = { value }
+	const setting = buildSetting('distribution', value)
+	produceSetting(setting)
 	await postDistribution(object)
 	return true
 }
 
 const setRout = async value => {
 	const object = { value }
+	const setting = buildSetting('routing', value)
+	produceSetting(setting)
 	await postRouting(object)
 	return true
 }
 
 const setDrones = async value => {
 	const object = { value }
+	const setting = buildSetting('drones', value)
+	produceSetting(setting)
 	await postDrones(object)
 	return true
+}
+
+const buildSetting = (setting, value) => {
+	return {
+		setting,
+		value,
+	}
 }
 
 export { settings, setDist, setRout, setDrones }
